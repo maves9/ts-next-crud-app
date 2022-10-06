5d95ccd7aca6d6a04126f6c308ede2efcd8eeb85
Martin Vestergaard <martin.vestergaard@team.blue>
2022-10-06 23:12:17 +0200

    clean logs

--- components/Create.tsx
+++ components/Create.tsx
@@ -10,11 +10,9 @@ const Create = ({ moreIdiots }) => {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(state),
-            });
-            const newData = await response.json()
+            })
 
-            console.log(newData);
-            
+            const newData = await response.json()
 
             moreIdiots(newData)
         } catch (error) {
